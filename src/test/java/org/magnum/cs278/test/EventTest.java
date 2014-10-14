61f642693e971abecbe317b341cc6ea103de9c91
grandyang <xiaochenyang@XiaochendeMacBook-Pro.local>
2014-10-14 12:07:54 -0500

    add Event test

--- src/test/java/org/magnum/cs278/test/EventTest.java
+++ src/test/java/org/magnum/cs278/test/EventTest.java
@@ -1,14 +1,24 @@
 package org.magnum.cs278.test;
 
+import java.util.List;
+
+import org.magnum.cs278.testdriven.*;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class EventTest {
-
+	
+	private App app = new App();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws Exception {
+		List<Event> pubs = app.getEventsForMonth("Mar-2014");
+		assertTrue(pubs.size() > 0); // At least one March-2014 event.
+		for(Event temp : pubs) {
+			assertEquals(temp.getMonth(),"Mar-2014");
+		}
 	}
 
 }
