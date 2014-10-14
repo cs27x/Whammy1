d1e1f02fd4236ae8013a1e732d2b52586bb22d04
Fangzhou Sun <fzsun316@gmail.com>
2014-10-14 12:17:39 -0500

    tests

--- src/test/java/org/magnum/cs278/test/AppTest.java
+++ src/test/java/org/magnum/cs278/test/AppTest.java
@@ -2,7 +2,12 @@ package org.magnum.cs278.test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.joda.time.DateTime;
 import org.junit.Test;
+import org.magnum.cs278.testdriven.App;
+import org.magnum.cs278.testdriven.Event;
 
 public class AppTest {
 
@@ -10,5 +15,83 @@ public class AppTest {
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	private App app = new App();
+	
+	@Test
+	public void testGetThreeThingsToDo() throws Exception {
+		List<Event> whatToDo = app.getThreeThingsToDo();
+		assertEquals(3, whatToDo.size());
+		
+		DateTime today = DateTime.now();
+		
+		for(Event thingToDo : whatToDo){
+			assertNotNull(thingToDo);
+			assertNotNull(thingToDo.getDate());
+			
+			try{
+				DateTime eventDate = Event.DATE_TIME_FORMAT.parseDateTime(thingToDo.getDate());
+				assertTrue(eventDate.isAfter(today));
+			}catch(IllegalArgumentException arg){
+			}
+		}
+	}
+	
+	@Test
+	public void testGetParkSpecialPermits() throws Exception {
+		List<Event> events = app.getParkSpecialPermits();
+		assertTrue(events.size() > 0);
+		for(Event event : events){
+			assertNotNull(event);
+			assertNotNull(event.getLocation());
+			assertNotNull(event.getName());
+			assertNotNull(event.getAttendance());
+			assertNotNull(event.getDate());
+		}
+	}
+	
+	
+	@Test
+	public void testGetEventsWithLocation() throws Exception {
+		List<Event> events = app.getEventsWithLocation("riverfront park");
+        assertTrue(events.size() > 0);
+		for(Event event : events) {
+			assertTrue(event.getLocation().toLowerCase().equals("riverfront park"));
+		}
+	}
 
+
+	@Test
+	public void testTodaysEvents() throws Exception {
+		List<Event> whatToDo = app.getTodaysEvents();
+		
+		for(Event thingToDo : whatToDo){
+			assertNotNull(thingToDo);
+			assertNotNull(thingToDo.getDate());
+			
+			try{
+				DateTime eventDate = Event.DATE_TIME_FORMAT.parseDateTime(thingToDo.getDate());
+				assertTrue(eventDate.isEqualNow());
+			}catch(IllegalArgumentException arg){
+			}
+		}
+	}
+	
+	@Test
+	public void testGetEventsForMonth() throws Exception {
+		
+		List<Event> events = app.getEventsForMonth("Mar-2014");
+		assertTrue(events.size() == 1);
+		assertEquals("Mar-2014", events.get(0).getMonth());
+	}
+	
+	@Test
+	public void testGetFirstEventOfMonth() throws Exception {
+		String month = "Feb-2014";
+		String testEventName = "Cupid's Chase";
+		
+		Event first = app.getFirstEventOfMonth(month);
+		
+		assertTrue(first.getName().equals(testEventName));
+	}
 }
