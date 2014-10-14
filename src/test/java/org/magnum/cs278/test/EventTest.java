85cab33eef6b4f4a10d58ba51e0eb78d0ad3fb3f
AToMic1993 <alex.t.meyer@vanderbilt.edu>
2014-10-14 12:04:33 -0500

    EventTest class testing and class structure testing

--- src/test/java/org/magnum/cs278/test/EventTest.java
+++ src/test/java/org/magnum/cs278/test/EventTest.java
@@ -1,14 +1,19 @@
 package org.magnum.cs278.test;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import org.magnum.cs278.testdriven.Event;
 
 public class EventTest {
+	private Event testEvent = new Event("name", "location", "0", "1", "1/1/2001");
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testToString() {
+		assertTrue(testEvent.toString().equals("name / location / 0 / 1 / 1/1/2001"));
 	}
-
+	
+	@Test
+	public void testGetDateTime() {
+		assertTrue(testEvent.getDateTime().equals(Event.DATE_TIME_FORMAT.parseDateTime("1/1/2001")));
+	}
 }
