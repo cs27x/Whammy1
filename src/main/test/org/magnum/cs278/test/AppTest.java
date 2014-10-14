8243a8ec98718d41613e97d7d00983a8f8ca666a
AToMic1993 <alex.t.meyer@vanderbilt.edu>
2014-10-14 11:17:25 -0500

    Added AppTest test file

---
+++ src/main/test/org/magnum/cs278/test/AppTest.java
@@ -0,0 +1,14 @@
+package org.magnum.cs278.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
