65fbaf115c3fdf185823ffcd0e59b2b83eb9b249
AToMic1993 <alex.t.meyer@vanderbilt.edu>
2014-10-14 11:14:52 -0500

    File structure and base test

---
+++ src/main/test/org/magnum/cs278/test/EventTest.java
@@ -0,0 +1,14 @@
+package org.magnum.cs278.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EventTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
